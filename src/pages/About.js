01594d8677dd63add0fbf8b47e959fed471964cb
zmainsk <skkim@j2stechno.com>
2023-05-16 14:40:51 +0900

    react-router-base

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const About = () => {
+  return (
+    <div>
+      <h1>소개</h1>
+      <p>리액트 라우터를 사용해 보는 프로젝트입니다.</p>
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+export default About;
