404c4c2c448d963512e959c241d10fa429a0b8ac
rudsa <rudsa@naver.com>
2023-05-28 15:46:04 +0900

    react-router-end

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import About from "./pages/About";
 import Article from "./pages/Article";
 import Articles from "./pages/Articles";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import MyPage from "./pages/MyPage";
+import NotFound from "./pages/NotFound";
 import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <Routes>
-      <Route element={<Layout />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="myPage" element={<MyPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
